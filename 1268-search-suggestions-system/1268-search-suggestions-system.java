cadfdd137a57c2b4662c3f7d4a26025159563968
Kushagra Tandon <kushagratan0107@gmail.com>
2026-07-24 00:26:01 +0530

    Time: 9 ms (90.94%), Space: 49.8 MB (58.3%) - LeetHub

---
+++ 1268-search-suggestions-system/1268-search-suggestions-system.java
@@ -0,0 +1,31 @@
+class Solution {
+    public List<List<String>> suggestedProducts(String[] products, String searchWord) {
+
+        List<List<String>> suggestionWords = new ArrayList<>();
+
+        Arrays.sort(products);
+
+        int left = 0, right = products.length - 1;
+        int size = products.length;
+
+        for(int i = 0; i < searchWord.length(); i++){
+            char ch = searchWord.charAt(i);
+
+            while(left <= right && (i >= products[left].length() || products[left].charAt(i) != ch)){
+                left += 1;
+            }
+
+            while(left <= right && (i >= products[right].length() || products[right].charAt(i) != ch)){
+                right -= 1;
+            }
+            
+            int index = left;
+            List<String> list = new ArrayList<>();
+            for(int start = left; start < Math.min(left + 3, right + 1); start++){
+                list.add(products[start]);
+            }
+            suggestionWords.add(list);
+        } 
+        return suggestionWords;
+    }
+}
