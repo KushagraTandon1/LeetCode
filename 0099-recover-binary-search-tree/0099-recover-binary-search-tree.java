12aac1d6e5b7b4df2c9e0a3e509fd8a73adcbaa1
Kushagra Tandon <kushagratan0107@gmail.com>
2026-07-28 00:40:48 +0530

    Time: 1 ms (100%), Space: 46.4 MB (70.96%) - LeetHub

--- 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
+++ 0099-recover-binary-search-tree/0099-recover-binary-search-tree.java
@@ -1,37 +1,24 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
 class Solution {
-    int index = 0;
-    public void buildInOrder(TreeNode root, List<Integer> inOrder){
+     TreeNode prev = new TreeNode(Integer.MIN_VALUE);
+        TreeNode first = null;
+        TreeNode second = null;
+    public void recover(TreeNode root){
         if(root == null) return;
-        buildInOrder(root.left, inOrder);
-        inOrder.add(root.val);
-        buildInOrder(root.right, inOrder);
-    }
-    public void recovermyTree(TreeNode root, List<Integer> inOrder){
-        if(root == null) return;
-        recovermyTree(root.left, inOrder);
-        root.val = inOrder.get(index);
-        index++;
-        recovermyTree(root.right, inOrder);
+        recover(root.left);
+        if(first == null && prev.val > root.val){
+            first = prev;
+        }
+        if(first != null && prev.val > root.val){
+            second = root;
+        }
+        prev = root;
+        recover(root.right);
     }
     public void recoverTree(TreeNode root) {
-        List<Integer> inOrder = new ArrayList<>();
-        buildInOrder(root, inOrder);
-        Collections.sort(inOrder);
-        recovermyTree(root, inOrder);
+        recover(root);
+        int temp = first.val;
+        first.val = second.val;
+        second.val = temp;
+        
     }
 }
