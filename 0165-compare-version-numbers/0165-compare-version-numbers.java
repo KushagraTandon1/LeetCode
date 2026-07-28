4be3a93b52e9bc77c5ce83d0d724328840946ae5
Kushagra Tandon <kushagratan0107@gmail.com>
2026-07-28 21:20:25 +0530

    Time: 4 ms (3.15%), Space: 43.6 MB (10.36%) - LeetHub

---
+++ 0165-compare-version-numbers/0165-compare-version-numbers.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int getIntegerValueOfString(String versionNumber){
+        return Integer.parseInt(versionNumber);
+    }
+    public int compareVersion(String version1, String version2) {
+        String [] ver1 = version1.split("[.]");
+        String [] ver2 = version2.split("[.]");
+        int size = Math.max(ver1.length, ver2.length);
+        for(int i = 0; i < size; i++){
+            int versionNumber1 = i < ver1.length ? getIntegerValueOfString(ver1[i]) : 0;
+            int versionNumber2 = i < ver2.length ? getIntegerValueOfString(ver2[i]) : 0;
+            if(versionNumber1 == versionNumber2) continue;
+            else if(versionNumber1 > versionNumber2) return 1;
+            else if(versionNumber1 < versionNumber2) return -1;
+        }
+        return 0;
+    }
+}
