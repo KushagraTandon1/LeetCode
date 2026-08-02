83443c8676e243803cf27a592850dcb3fe99b5a5
Kushagra Tandon <kushagratan0107@gmail.com>
2026-08-02 14:47:24 +0530

    Time: 329 ms (68.83%), Space: 0B (100%) - LeetHub

---
+++ 3657-find-loyal-customers/3657-find-loyal-customers.sql
@@ -0,0 +1,14 @@
+SELECT 
+    customer_id
+FROM 
+    customer_transactions
+GROUP BY 
+    customer_id
+HAVING 
+        COUNT(*) >= 3 
+    AND     
+        DATEDIFF(MAX(transaction_date), MIN(transaction_date))>= 30 
+    AND 
+        SUM(IF(transaction_type = 'refund', 1, 0))/COUNT(*) * 100 < 20
+ORDER BY 
+    customer_id;
